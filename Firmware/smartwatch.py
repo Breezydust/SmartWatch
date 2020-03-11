23bff5afc76595411df72f38884a69728cd1d027
Breezydust <thomas.aziz13@gmail.com>
2020-03-10 21:06:48 -0400

    uploaded firmware files

---
+++ Firmware/smartwatch.py
@@ -0,0 +1,102 @@
+#Library imports
+import time
+import board
+import busio
+import threading
+import adafruit_adxl34x
+import adafruit_tmp006
+import adafruit_ads1x15.ads1015
+
+#i2c initialization
+i2c = busio.I2C(board.SCL, board.SDA)
+
+#accelerometer initialization
+accel = adafruit_adxl34x.ADXL345(i2c)
+
+#adc initialization
+from adafruit_ads1x15.analog_in import AnalogIn
+adc = adafruit_ads1x15.ADS1015(i2c)
+
+#temp initialization
+temp = adafruit_tmp006.TMP006(i2c)
+
+
+while True:
+    #print acceleration data
+    print("Acceleration - X: %f Y: %f Z: %f"%accel.acceleration) 
+    if bool(accel.events["freefall"]): print("Sensor dropped!")
+    if bool(accel.events["tap"]): print("Sensor tapped!")
+    if bool(accel.events["motion"]): print("Sensor moved!")
+
+    #print pulse
+    Signal = adc.read_adc(0, gain=GAIN)   #ADC channel 0
+    curTime = int(time.time()*1000)
+
+    sampleCounter += curTime - lastTime;      #                   # keep track of the time in mS with this variable
+    lastTime = curTime
+    N = sampleCounter - lastBeatTime;     #  # monitor the time since the last beat to avoid noise
+    #print N, Signal, curTime, sampleCounter, lastBeatTime
+
+    ##  find the peak and trough of the pulse wave
+    if Signal < thresh and N > (IBI/5.0)*3.0 :  #       # avoid dichrotic noise by waiting 3/5 of last IBI
+        if Signal < T :                        # T is the trough
+          T = Signal;                         # keep track of lowest point in pulse wave
+
+    if Signal > thresh and  Signal > P:           # thresh condition helps avoid noise
+        P = Signal;                             # P is the peak
+                                            # keep track of highest point in pulse wave
+
+      #  NOW IT'S TIME TO LOOK FOR THE HEART BEAT
+      # signal surges up in value every time there is a pulse
+    if N > 250 :                                   # avoid high frequency noise
+        if  (Signal > thresh) and  (Pulse == False) and  (N > (IBI/5.0)*3.0)  :
+          Pulse = True;                               # set the Pulse flag when we think there is a pulse
+          IBI = sampleCounter - lastBeatTime;         # measure time between beats in mS
+          lastBeatTime = sampleCounter;               # keep track of time for next pulse
+
+          if secondBeat :                        # if this is the second beat, if secondBeat == TRUE
+            secondBeat = False;                  # clear secondBeat flag
+            for i in range(0,10):             # seed the running total to get a realisitic BPM at startup
+              rate[i] = IBI;
+
+          if firstBeat :                        # if it's the first time we found a beat, if firstBeat == TRUE
+            firstBeat = False;                   # clear firstBeat flag
+            secondBeat = True;                   # set the second beat flag
+            continue                              # IBI value is unreliable so discard it
+
+
+          # keep a running total of the last 10 IBI values
+          runningTotal = 0;                  # clear the runningTotal variable
+
+          for i in range(0,9):                # shift data in the rate array
+            rate[i] = rate[i+1];                  # and drop the oldest IBI value
+            runningTotal += rate[i];              # add up the 9 oldest IBI values
+
+          rate[9] = IBI;                          # add the latest IBI to the rate array
+          runningTotal += rate[9];                # add the latest IBI to runningTotal
+          runningTotal /= 10;                     # average the last 10 IBI values
+          BPM = 60000/runningTotal;               # how many beats can fit into a minute? that's BPM!
+          print 'BPM: {}'.format(BPM)
+
+    if Signal < thresh and Pulse == True :   # when the values are going down, the beat is over
+        Pulse = False;                         # reset the Pulse flag so we can do it again
+        amp = P - T;                           # get amplitude of the pulse wave
+        thresh = amp/2 + T;                    # set thresh at 50% of the amplitude
+        P = thresh;                            # reset these for next time
+        T = thresh;
+
+    if N > 2500 :                          # if 2.5 seconds go by without a beat
+        thresh = 512;                          # set thresh default
+        P = 512;                               # set P default
+        T = 512;                               # set T default
+        lastBeatTime = sampleCounter;          # bring the lastBeatTime up to date
+        firstBeat = True;                      # set these to avoid noise
+        secondBeat = False;                    # when we get the heartbeat back
+        print "no beats found"
+        
+    #print temp
+    temp.begin()
+    print("Temperature: %f"%temp.readObjTempC())
+
+    time.sleep(1)
+
